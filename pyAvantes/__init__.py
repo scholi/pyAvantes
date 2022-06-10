e0e75a286ecc7f395a1551b73cddeb24d88e84be
scholi <olivier.scholder@fluxim.com>
2022-06-10 22:39:37 +0200

    clean up

--- pyAvantes/__init__.py
+++ pyAvantes/__init__.py
@@ -8,7 +8,7 @@ from __future__ import absolute_import
 from .raw8 import Raw8
 
 __all__ = ["raw8"]
-__version__ = '0.0.2'
+__version__ = '0.0.3'
 __author__ = 'Olivier Scholder'
 __copyright__ = "Copyright 2022, O. Scholder, Zürich, Switzerland"
 __email__ = "o.scholder@gmail.com"
